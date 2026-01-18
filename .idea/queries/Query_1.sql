38679ab1c5fc14e210944d4a8a2bb399201f12cb
GedMarc <ged_marc@hotmail.com>
2026-01-18 09:56:56 +0200

    Telemetry Beginnings

--- .idea/queries/Query_1.sql
+++
@@ -1,6 +0,0 @@
-create view public.received_barcodes as
-SELECT DISTINCT exet.value
-   FROM event.event e
-     JOIN event.eventxeventtype exet ON e.eventid = exet.eventid
-     JOIN event.eventtype et ON et.eventtypeid = exet.eventtypeid
-  WHERE et.eventtypedesc = 'BarcodeReceived'
