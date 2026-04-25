f7de6f6e03240da2d1dc98a0430713eab144ac4c
GedMarc <ged_marc@hotmail.com>
2026-04-25 16:30:52 +0200

    Update to RC9

--- bduild.ps1
+++ bduild.ps1
@@ -1,6 +1,6 @@
 mvn source:jar install `
   "-DskipTests" `
   "-Dmaven.javadoc.skip=false" `
-  "-Pboms,guicedee,services,entityassist,jwebmp,activity-master" `
+  "-Pguicedee-boms,jwebmp-boms,guicedee,services,entityassist,jwebmp,activity-master" `
   -T 8 `
   @args
--- clean.ps1
+++ clean.ps1
@@ -1,4 +1,4 @@
-mvn source:jar install `
+mvn clean `
   "-DskipTests" `
   "-Dmaven.javadoc.skip=true" `
   "-Pguicedee-boms,jwebmp-boms,guicedee,services,entityassist,jwebmp,activity-master" `
